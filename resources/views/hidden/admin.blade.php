066417bf82976450a61f2e5590dd505a23fda8ac
2000Nic <nicolas.magnussen@hotmail.com>
2020-04-20 21:36:08 +0200

    Update admin.blade.php

--- resources/views/hidden/admin.blade.php
+++ resources/views/hidden/admin.blade.php
@@ -17,11 +17,11 @@
     </a>
     <a href="/admin/places" class="card text-center col-sm-3 d-inline-block mx-3 my-3" style="height: 20vh;">
         <br><br><h2>Manage Place</h2>
-        <h2 h2 style="font-size: 4em;"><i class="fas fa-thumbtack"></i></h2>
+        <h2 style="font-size: 4em;"><i class="fas fa-thumbtack"></i></h2>
     </a>
     <a href="/admin/fleet" class="card text-center col-sm-3 d-inline-block mx-3 my-3" style="height: 20vh;">
         <br><br><h2>Manage Fleet</h2>
-        <h2 h2 style="font-size: 4em;"><i class="fas fa-paper-plane"></i></h2>
+        <h2 style="font-size: 4em;"><i class="fas fa-paper-plane"></i></h2>
     </a>
     <a href="/admin/posts" class="card text-center col-sm-3 d-inline-block mx-3 my-3" style="height: 20vh;">
         <br><br><h2>Manage Posts</h2>
