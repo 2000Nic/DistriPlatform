f58c75829c93a73a5cc9eab60f673ae3b289dcaf
2000Nic <nicolas.magnussen@hotmail.com>
2020-04-29 12:32:40 +0200

    Removed places page from admin and management panels

--- resources/views/hidden/admin/index.blade.php
+++ resources/views/hidden/admin/index.blade.php
@@ -15,10 +15,6 @@
         <br><br><h2>Manage Cards</h2>
         <h2 style="font-size: 4em;"><i class="fas fa-toilet-paper"></i></h2>
     </a>
-    <a href="/admin?page=places" class="card text-center col-sm-3 d-inline-block mx-3 my-3" style="height: 20vh;">
-        <br><br><h2>Manage Place</h2>
-        <h2 style="font-size: 4em;"><i class="fas fa-thumbtack"></i></h2>
-    </a>
     <a href="/admin?page=fleet" class="card text-center col-sm-3 d-inline-block mx-3 my-3" style="height: 20vh;">
         <br><br><h2>Manage Fleet</h2>
         <h2 style="font-size: 4em;"><i class="fas fa-paper-plane"></i></h2>
